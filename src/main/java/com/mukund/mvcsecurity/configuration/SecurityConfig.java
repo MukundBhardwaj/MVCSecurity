4d4281576eace684fc998299fa0a3bf5ecf92229
MukundBhardwaj <mukund760@gmail.com>
2023-06-27 09:40:05 +0530

    Enabling method level security

--- src/main/java/com/mukund/mvcsecurity/configuration/SecurityConfig.java
+++ src/main/java/com/mukund/mvcsecurity/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter.HeaderValue;
 
 @Configuration
+@EnableMethodSecurity
 /**
  * Security Config class for defining beans customizing Spring Security
  * 
@@ -20,52 +22,57 @@ import org.springframework.security.web.header.writers.XXssProtectionHeaderWrite
  */
 public class SecurityConfig {
 
-    @Bean
-    /**
-     * Bean method to get default encoder for passwords
-     * 
-     * @return {@code PasswordEncoder} default password encoder object
-     */
-    PasswordEncoder getEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        /**
+         * Bean method to get default encoder for passwords
+         * 
+         * @return {@code PasswordEncoder} default password encoder object
+         */
+        PasswordEncoder getEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    /**
-     * Bean method to get {@code AuthenticationProvider}
-     * 
-     * @param userDetailsService {@code UserDetailsService} Service Object used for
-     *                           Spring Security
-     * @param passwordEncoder    {@code PasswordEncoder} Object used for password
-     *                           matching
-     * @return {@code AuthenticationProvider} default authentication provider object
-     */
-    AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
-            PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(passwordEncoder);
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        return authenticationProvider;
-    }
+        @Bean
+        /**
+         * Bean method to get {@code AuthenticationProvider}
+         * 
+         * @param userDetailsService {@code UserDetailsService} Service Object used for
+         *                           Spring Security
+         * @param passwordEncoder    {@code PasswordEncoder} Object used for password
+         *                           matching
+         * @return {@code AuthenticationProvider} default authentication provider object
+         */
+        AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
+                        PasswordEncoder passwordEncoder) {
+                DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(passwordEncoder);
+                authenticationProvider.setUserDetailsService(userDetailsService);
+                return authenticationProvider;
+        }
 
-    @Bean
-    /**
-     * Bean method for {@code SecurityFilterChain}
-     * 
-     * @param http {@code HttpSecurity} Spring Security http security object
-     * @return {@code SecurityFilterChain}
-     * @throws Exception
-     */
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin.loginProcessingUrl("/signin").defaultSuccessUrl("/", true))
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.deny())
-                        .httpStrictTransportSecurity(
-                                hstsConfig -> hstsConfig.includeSubDomains(true).maxAgeInSeconds(315536000))
-                        .xssProtection(
-                                xssProtectionConfig -> xssProtectionConfig.headerValue(HeaderValue.ENABLED_MODE_BLOCK))
-                        .contentSecurityPolicy(
-                                contentSecurityPolicy -> contentSecurityPolicy.policyDirectives("default-src 'self';")))
-                .cors(corsConfig -> corsConfig.disable())
-                .build();
-    }
+        @Bean
+        /**
+         * Bean method for {@code SecurityFilterChain}
+         * 
+         * @param http {@code HttpSecurity} Spring Security http security object
+         * @return {@code SecurityFilterChain}
+         * @throws Exception
+         */
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                return http.authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                                .formLogin(formLogin -> formLogin.loginProcessingUrl("/signin").defaultSuccessUrl("/",
+                                                true))
+                                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.deny())
+                                                .httpStrictTransportSecurity(
+                                                                hstsConfig -> hstsConfig.includeSubDomains(true)
+                                                                                .maxAgeInSeconds(315536000))
+                                                .xssProtection(
+                                                                xssProtectionConfig -> xssProtectionConfig.headerValue(
+                                                                                HeaderValue.ENABLED_MODE_BLOCK))
+                                                .contentSecurityPolicy(
+                                                                contentSecurityPolicy -> contentSecurityPolicy
+                                                                                .policyDirectives(
+                                                                                                "default-src 'self';")))
+                                .cors(corsConfig -> corsConfig.disable())
+                                .build();
+        }
 }
