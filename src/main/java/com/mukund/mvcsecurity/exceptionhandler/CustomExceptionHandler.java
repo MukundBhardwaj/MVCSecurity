abecd2c5072c94d2b69db844f4d415c8185c9386
MukundBhardwaj <mukund760@gmail.com>
2023-06-27 09:40:27 +0530

    adding method argument exception handler

--- src/main/java/com/mukund/mvcsecurity/exceptionhandler/CustomExceptionHandler.java
+++ src/main/java/com/mukund/mvcsecurity/exceptionhandler/CustomExceptionHandler.java
@@ -2,11 +2,14 @@ package com.mukund.mvcsecurity.exceptionhandler;
 
 import java.time.ZonedDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,6 +30,18 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
                         new ErrorResponse(404, "Not Found", request.getRequestURI(), ZonedDateTime.now(),
                                 ex.getMessage(),
                                 null));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return ResponseEntity.status(status)
+                .headers(headers).body(
+                        new ErrorResponse(status.value(), status.toString().split("\s", 2)[1],
+                                ((HttpServletRequest) request).getRequestURI(),
+                                ZonedDateTime.now(),
+                                ex.getMessage(),
+                                ex.getAllErrors().stream().map(error -> error.getDefaultMessage()).toList()));
     }
 
 }
