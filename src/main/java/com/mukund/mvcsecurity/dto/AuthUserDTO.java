a56f37c2a5909825c22294295dec9d880b221987
MukundBhardwaj <mukund760@gmail.com>
2023-06-27 09:41:10 +0530

    Creating Separate DTO for create and update

--- src/main/java/com/mukund/mvcsecurity/dto/AuthUserDTO.java
+++ src/main/java/com/mukund/mvcsecurity/dto/AuthUserDTO.java
@@ -7,16 +7,30 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 /**
- * Data Transfer Object record for AuthUser
+ * Data Transfer Object class for AuthUser containing record for creating and
+ * updating
  * 
  * @since 1.0
  * @author MukundBhardwaj
  */
-public record AuthUserDTO(
+public final class AuthUserDTO {
+        public static record CreateUserDTO(
 
-        @NotEmpty(message = "name cannot be empty") String name,
-        @NotEmpty(message = "email cannot be empty") String email,
-        @NotEmpty(message = "password cannot be empty") @JsonProperty(access = Access.WRITE_ONLY) String password,
-        @NotNull(message = "active cannot be null") Boolean active) {
+                        @NotEmpty(message = "name cannot be empty") String name,
+                        @NotEmpty(message = "email cannot be empty") String email,
+                        @NotEmpty(message = "password cannot be empty") @JsonProperty(access = Access.WRITE_ONLY) String password,
+                        @NotNull(message = "active cannot be null") Boolean active) {
 
+        }
+
+        public static record UpdateUserDTO(
+
+                        @NotEmpty(message = "name cannot be empty") String name,
+                        @NotEmpty(message = "password cannot be empty") @JsonProperty(access = Access.WRITE_ONLY) String password,
+                        @NotNull(message = "active cannot be null") Boolean active) {
+
+        }
+
+        private AuthUserDTO() {
+        }
 }
