5749a68a442caae8f1bb2f1144bf2d8e4f394c9b
MukundBhardwaj <mukund760@gmail.com>
2023-06-27 23:54:09 +0530

    Adding java doc comments

--- src/main/java/com/mukund/mvcsecurity/MVCSecurityApplication.java
+++ src/main/java/com/mukund/mvcsecurity/MVCSecurityApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * Springboot Application with Spring Security
+ * 
+ * @version 1.0
+ * @author Mukund Bhardwaj
+ */
 public class MVCSecurityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mukund/mvcsecurity/service/AuthUserDetailsService.java
+++ src/main/java/com/mukund/mvcsecurity/service/AuthUserDetailsService.java
@@ -3,22 +3,71 @@ package com.mukund.mvcsecurity.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.mukund.mvcsecurity.dto.AuthUserDTO;
 import com.mukund.mvcsecurity.exceptionhandler.ResourceNotFoundException;
 import com.mukund.mvcsecurity.model.AuthUserDetails;
 
+/**
+ * Service interface for {@code AuthUserDetails}
+ * 
+ * @since 1.0
+ * @author Mukund Bhardwaj
+ */
 public interface AuthUserDetailsService extends UserDetailsService {
 
+    @Override
+    /**
+     * Method to get user by username
+     * 
+     * @param username username for user to load
+     * @return {@code UserDetails}
+     */
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+    /**
+     * Method to get all the list of users
+     * 
+     * @return List<? extends AuthUserDetails>
+     */
     public List<? extends AuthUserDetails> findAllUsers();
 
+    /**
+     * Method to create user
+     * 
+     * @param authUserDTO {@code AuthUserDTO.createUserDTO}
+     * @return {@code AuthUserDetails}
+     */
     public AuthUserDetails createUser(AuthUserDTO.CreateUserDTO authUserDTO);
 
+    /**
+     * Method to get user by user ID
+     * 
+     * @param id UUID user ID
+     * @return {@code AuthUserDetails}
+     * @throws ResourceNotFoundException
+     */
     public AuthUserDetails getUserByID(UUID id) throws ResourceNotFoundException;
 
+    /**
+     * Method to update user
+     * 
+     * @param id          UUID user ID
+     * @param authUserDTO {@code AuthUserDTO.UpdateUserDTO}
+     * @return {@code AuthUserDetails}
+     * @throws ResourceNotFoundException
+     */
     public AuthUserDetails updateUser(UUID id, AuthUserDTO.UpdateUserDTO authUserDTO) throws ResourceNotFoundException;
 
+    /**
+     * Method to delete user using ID
+     * 
+     * @param id UUID user ID
+     * @throws ResourceNotFoundException
+     */
     public void deleteUserByID(UUID id) throws ResourceNotFoundException;
 
 }
